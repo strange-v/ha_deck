f4d3c5e67c34bfbeca5ddd5397940d7ad8caafcc
Allen Bauer <kylix.rd@gmail.com>
2024-01-21 15:56:09 -0800

    Format & code updates,

--- components/waveshare_s3_touch_lcd/hd_device_s3_Touch_LCD_43.cpp
+++ components/waveshare_s3_touch_lcd/hd_device_s3_Touch_LCD_43.cpp
@@ -21,7 +21,6 @@ namespace hd_device {
 
 static const char *const TAG = "HD_DEVICE";
 static lv_disp_draw_buf_t draw_buf;
-//static lv_color_t *buf = (lv_color_t *)heap_caps_malloc(LVGL_BUF_SIZE * sizeof(lv_color_t), MALLOC_CAP_DMA);
 
 ESP_Panel panel;
 
@@ -31,25 +30,13 @@ unsigned long flush_time = 0;
 
 void IRAM_ATTR flush_pixels(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p)
 {
-    //uint32_t w = (area->x2 - area->x1 + 1);
-    //uint32_t h = (area->y2 - area->y1 + 1);
-    //uint32_t len = w * h;
-
-    auto* display = panel.getLcd();
-    display->drawBitmap(area->x1, area->y1, area->x2, area->y2, color_p);
-    
-    // lcd.startWrite();                            /* Start new TFT transaction */
-    // lcd.setAddrWindow(area->x1, area->y1, w, h); /* set the working window */
-    // lcd.writePixels((uint16_t *)&color_p->full, len, true);
-    // lcd.endWrite();                              /* terminate TFT transaction */
-
+    panel.getLcd()->drawBitmap(area->x1, area->y1, area->x2, area->y2, color_p);
     lv_disp_flush_ready(disp);
 }
 
 void IRAM_ATTR touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data)
 {
     auto* touch = panel.getLcdTouch();
-
     touch->readData();
     if (touch->getTouchState()) {
         TouchPoint point = touch->getPoint();
@@ -63,19 +50,18 @@ void IRAM_ATTR touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data
 
 void HaDeckDevice::setup() {
     ESP_LOGCONFIG(TAG, "HaDeckDevice::Setup()");
+
     lv_init();
-    lv_theme_default_init(NULL, lv_color_hex(0xFFEB3B), lv_color_hex(0xFF7043), 1, LV_FONT_DEFAULT);
+    //lv_theme_default_init(NULL, lv_color_hex(0xFFEB3B), lv_color_hex(0xFF7043), 1, LV_FONT_DEFAULT);
 
     uint8_t* buf = (uint8_t *)heap_caps_calloc(1, LVGL_BUF_SIZE * sizeof(lv_color_t), MALLOC_CAP_INTERNAL);
 
-    lv_disp_draw_buf_init(&draw_buf, buf, nullptr, TFT_WIDTH * 20);
+    lv_disp_draw_buf_init(&draw_buf, buf, nullptr, LVGL_BUF_SIZE);
 
     static lv_disp_drv_t disp_drv;
     lv_disp_drv_init(&disp_drv);
-    disp_drv.hor_res = TFT_WIDTH;
-    disp_drv.ver_res = TFT_HEIGHT;
-    disp_drv.rotated = 0;
-    disp_drv.sw_rotate = 0;
+    disp_drv.hor_res = ESP_PANEL_LCD_H_RES;
+    disp_drv.ver_res = ESP_PANEL_LCD_V_RES;
     disp_drv.flush_cb = flush_pixels;
     disp_drv.draw_buf = &draw_buf;
     lv_disp_t *disp = lv_disp_drv_register(&disp_drv);
