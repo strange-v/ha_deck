f15153345a6d3c9fe4ff65c95245f3b292143c47
Allen Bauer <kylix.rd@gmail.com>
2024-01-21 19:34:58 -0800

    Compile error.

--- components/waveshare_s3_touch_lcd/hd_device_s3_Touch_LCD_43.cpp
+++ components/waveshare_s3_touch_lcd/hd_device_s3_Touch_LCD_43.cpp
@@ -156,7 +156,7 @@ void lvglDeck::loop() {
     }
 }
 
-void lvglDeck::set_brightness(unit8_t value) {
+void lvglDeck::set_brightness(uint8_t value) {
     auto* backlight = panel.getBacklight();
     if (backlight) {
       backlight->setBrightness(brightness_);
